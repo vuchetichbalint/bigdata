2872631202d8a9c87c5f3de8c2e55d3e48b789a7
Vuchetich Balint <vuchetichbalint@gmail.com>
2015-12-04 17:24:36 +0100

    hazi fajlok

---
+++ python/shopperschallenge/mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+import datetime
+
+for line in sys.stdin:
+	line=line.split(",")
+    if line[0] != "":
+    	line[0] = line[0][:4] + '-'+ str(datetime.date(int(line[0][:4]), int(line[0][5:7]), int(line[0][8:10])).isocalendar()[1])
+        print('%s\t%d' % (line[0]+','+line[1], int(line[2]))
