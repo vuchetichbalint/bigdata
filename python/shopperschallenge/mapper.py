e97279769c5b84a108f3c23f97c9050c1ed58ee5
Vuchetich Balint <vuchetichbalint@gmail.com>
2015-12-04 18:13:40 +0100

    testesetek hozzaadva

--- python/shopperschallenge/mapper.py
+++ python/shopperschallenge/mapper.py
@@ -4,7 +4,22 @@ import sys
 import datetime
 
 for line in sys.stdin:
-	line=line.split(",")
+    line=line.split(",")
     if line[0] != "":
-    	line[0] = line[0][:4] + '-'+ str(datetime.date(int(line[0][:4]), int(line[0][5:7]), int(line[0][8:10])).isocalendar()[1])
-        print('%s\t%d' % (line[0]+','+line[1], int(line[2]))
+        line[0] = line[0][:4] + '-'+ str(datetime.date(int(line[0][:4]), int(line[0][5:7]), int(line[0][8:10])).isocalendar()[1])
+        print('%s\t%d' % (line[0]+','+line[1], int(line[2])))
+
+
+
+##--------------------------------------------------------------------------------------
+##test:
+
+#import sys
+#import datetime
+#data = {"2012-03-02,7,1","2012-03-02,63,1","2012-03-02,97,1","2012-03-02,25,1","2012-03-02,55,2","2012-03-02,97,1","2012-03-02,99,1","2012-03-02,59,1"}
+
+#for line in data:
+#    line=line.split(",")
+#    if line[0] != "":
+#        line[0] = line[0][:4] + '-'+ str(datetime.date(int(line[0][:4]), int(line[0][5:7]), int(line[0][8:10])).isocalendar()[1])
+#        print('%s\t%d' % (line[0]+','+line[1], int(line[2])))
--- python/shopperschallenge/reducer.py
+++ python/shopperschallenge/reducer.py
@@ -12,7 +12,7 @@ for line in sys.stdin:
         value = int(value)
         if key != lastkey:
             if lastkey is not None:
-                print '%s\t%d' % (lastkey, current)
+                print ('%s\t%d' % (lastkey, current))
             lastkey = key
             current = 0
         current += value
@@ -20,4 +20,35 @@ for line in sys.stdin:
         pass
 
 if lastkey is not None:
-    print '%s\t%d' % (lastkey,current)
+    print ('%s\t%d' % (lastkey,current))
+
+
+
+##--------------------------------------------------------------------------------------
+##test
+
+
+#import sys
+##data = {"2012-9,7	1","2012-9,7	1","2012-9,7	1","2012-9,25	2","2012-9,55	3","2012-9,59	3","2012-9,59	4","2012-9,63	5","2012-9,97	6","2012-9,97	1000","2012-9,99	7","2012-9,99	7"}
+#data = {"2012-9,7	1","2012-9,7	1","2012-9,7	1"}
+
+#lastkey = None
+#current = 0
+    
+#for line in data:
+#    line = line.strip()
+#    key, value = line.split('\t')
+#    try:
+#        value = int(value)
+#        if key != lastkey:
+#            if lastkey is not None:
+#                print ('%s\t%d' % (lastkey, current))
+#            lastkey = key
+#            current = 0
+#        current += value
+#    except ValueError:
+#        pass
+
+#if lastkey is not None:
+#    print ('%s\t%d' % (lastkey,current))
+
