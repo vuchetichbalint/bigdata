3ec49a5fab6d32fb44780082a2990ec84a4be00a
Vuchetich Balint <vuchetichbalint@gmail.com>
2015-12-04 18:23:51 +0100

    list->array

--- python/shopperschallenge/reducer.py
+++ python/shopperschallenge/reducer.py
@@ -24,31 +24,32 @@ if lastkey is not None:
 
 
 
+
+
 ##--------------------------------------------------------------------------------------
 ##test
 
 
-#import sys
-##data = {"2012-9,7	1","2012-9,7	1","2012-9,7	1","2012-9,25	2","2012-9,55	3","2012-9,59	3","2012-9,59	4","2012-9,63	5","2012-9,97	6","2012-9,97	1000","2012-9,99	7","2012-9,99	7"}
-#data = {"2012-9,7	1","2012-9,7	1","2012-9,7	1"}
+import sys
+data = ["2012-9,7	1","2012-9,7	1","2012-9,7	1","2012-9,25	2","2012-9,55	3","2012-9,59	3","2012-9,59	4","2012-9,63	5","2012-9,97	6","2012-9,97	1000","2012-9,99	7","2012-9,99	7"]
 
-#lastkey = None
-#current = 0
+lastkey = None
+current = 0
     
-#for line in data:
-#    line = line.strip()
-#    key, value = line.split('\t')
-#    try:
-#        value = int(value)
-#        if key != lastkey:
-#            if lastkey is not None:
-#                print ('%s\t%d' % (lastkey, current))
-#            lastkey = key
-#            current = 0
-#        current += value
-#    except ValueError:
-#        pass
+for line in data:
+   line = line.strip()
+   key, value = line.split('\t')
+   try:
+       value = int(value)
+       if key != lastkey:
+           if lastkey is not None:
+               print ('%s\t%d' % (lastkey, current))
+           lastkey = key
+           current = 0
+       current += value
+   except ValueError:
+       pass
 
-#if lastkey is not None:
-#    print ('%s\t%d' % (lastkey,current))
+if lastkey is not None:
+   print ('%s\t%d' % (lastkey,current))
 
